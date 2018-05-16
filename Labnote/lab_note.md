c6cdc020323c47ead8df96ef3c2b5eac6625d5f1
zyang21x <spadesacezy@hotmail.com>
2018-05-16 17:57:08 +0200

    reset function

--- Labnote/lab_note.md
+++ Labnote/lab_note.md
@@ -338,3 +338,18 @@ This method can help reduce CPU cost, accerlarate the loading speed.
 Finished combination work, eventually, everything seems works fine.
 
 Except from there is some bug in sunburst plot, when click really some children pathway, the radius cannot close itselves.
+
+----
+2018-5-14
+
+get some feedback from lask week's fika session, working on improving ux at present.
+
+Add the overall search function for the sunburst diagram.
+
+----
+2018-5-15
+
+Find out the reason of THE BUG, according to the book:
+
+      newer transitions interrupt and override older transitions. Clicking the bars initiates one transition. Immediately mousing over a bar interrupts that initial transition in order to run the mouseover highlight transition we specified earlier. The end result is that those moused-over bars never make it to their final destinations.
+      But don’t worry. This example is just a good argument for keeping hover effects in CSS, while letting D3 and JavaScript manage the more visually intensive actions.
--- STpathway_view/sunburst.js
+++ STpathway_view/sunburst.js
@@ -22,14 +22,11 @@ var colorScale = d3.scaleQuantize()
 
 var partition = d3.partition();
 
-
-
 var arc = d3.arc()
     .startAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x0))); })
     .endAngle(function(d) { return Math.max(0, Math.min(2 * Math.PI, x(d.x1))); })
     .innerRadius(function(d) { return Math.max(0, y(d.y0)); })
     .outerRadius(function(d) { return Math.max(0, y(d.y1)); });
-
 
 var svg = d3.select("body").append("svg")
     .attr("width", width)
@@ -45,6 +42,8 @@ var breadcrumb = d3.breadcrumb()
 var img= document.createElement("img");
 
 d3.select("body").append("input").attr("id", "searchid").attr("value", "HomoSapiens");
+
+
 
 //main fuction start
 d3.select("#json_sources").on('change', draw)
@@ -66,14 +65,13 @@ function draw() {
         roots = d3.hierarchy(root)
         roots.sum(function(d) { return d.children ? 0 : d.ngenes; });
 
-
-        var plot = svg.selectAll("path")
+          svg.selectAll("path")
            .data(partition(roots).descendants())
            .enter().append("path")
            .attr("d", arc)
            .style("fill", function(d) { return colorScale((d.children ? d : d.parent).data.explained_ratios); })
            .on("click", click)
-           .on('mouseover', mouseover)
+           //.on('mouseover', mouseover)
         .append("title")
           .text(function(d) { return "pathway name:  " + d.data.source + "\n" + "q:  " + parseFloat(d.data.explained_ratios) +
           "\n" + "description:  " + d.data.description })
@@ -83,40 +81,43 @@ function draw() {
           d3.selectAll("path")
               .style("stroke", "white")
               .style("stroke-width", 3)
-              .style("stroke-opacity", .4); 
+              .style("stroke-opacity", .4);
         var highlight = d3.selectAll("path")
          .filter(function(d) { return d.data.source === document.getElementById("searchid").value });
          highlight.transition()
          .duration(700)
          .style("stroke", "red")
-         .style("stroke-width", 7);}
+         .style("stroke-width", 7);};
+
+         document.getElementById("Reset").addEventListener("click", reset);
+         function reset(){svg.transition()
+         .duration(1000)
+         .tween("scale", function() {
+             var xd = d3.interpolate(x.domain(), [0, 0.31* Math.PI]),
+                 yd = d3.interpolate(y.domain(), [0, 0.31* Math.PI]),
+                 yr = d3.interpolate(y.range(), [0, radius]);
+             return function(t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); };
+         })
+         .selectAll("path")
+         .attrTween("d", function(d) { return function() { return arc(d); }; });
+         };
          });
-
-
-    function reset(){  };
-    document.getElementById("Reset").addEventListener("click", reset);
 
     function click(d) {
             svg.transition()
             .duration(1000)
-          .selectAll("path")
-            .attrTween("d", arcTween(d))
+            .tween("scale", function() {
+    						var xd = d3.interpolate(x.domain(), [d.x0, d.x1]),
+    								yd = d3.interpolate(y.domain(), [d.y0, 1]),
+    								yr = d3.interpolate(y.range(), [d.y0 ? 20 : 0, radius]);
+    						return function(t) { x.domain(xd(t)); y.domain(yd(t)).range(yr(t)); };
+    				})
+    				.selectAll("path")
+    				.attrTween("d", function(d) { return function() { return arc(d); }; });
             return datapick(d)};
 
-    function arcTween(d) {
-      var xd = d3.interpolate(x.domain(), [d.x0, d.x1]),
-          yd = d3.interpolate(y.domain(), [d.y0, 1]),
-          yr = d3.interpolate(y.range(), [d.y0 ? 20 : 0, radius]);
-        return function(d, i) {
-          return i
-              ? function(t) { return arc(d); }
-              : function(t) {
-                  x.domain( xd(t) );
-                  y.domain( yd(t) ).range( yr(t) );
-                  return arc(d);
-              };
-        };
-    };
+
+
     d3.select("#container").on("mouseleave", mouseleave)
     d3.select(self.frameElement).style("height", height + "px")
 };
